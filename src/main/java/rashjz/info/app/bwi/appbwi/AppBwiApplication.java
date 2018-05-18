e3066bb2613fad16bb095f9f05f2b69ef84e6ce4
Rashad Javadov <rashjz@users.noreply.github.com>
2018-05-18 16:17:01 +0200

    Update AppBwiApplication.java

--- src/main/java/rashjz/info/app/bwi/appbwi/AppBwiApplication.java
+++ src/main/java/rashjz/info/app/bwi/appbwi/AppBwiApplication.java
@@ -1,14 +1,38 @@
 package rashjz.info.app.bwi.appbwi;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import rashjz.info.app.bwi.appbwi.domain.OrderDetail;
+import rashjz.info.app.bwi.appbwi.service.RouteAndProcessService;
 
 @SpringBootApplication
 @IntegrationComponentScan
+@EnableJms
 public class AppBwiApplication {
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppBwiApplication.class, args);
 	}
+
+	@Bean
+	CommandLineRunner runner(RouteAndProcessService routeAndProcessService) {
+        return arg -> {
+
+			Message<OrderDetail> message = MessageBuilder.withPayload(OrderDetail.builder().amount(324).build())
+					.setHeader("messageHeader", "Message_Header1_Value")
+					.build();
+
+			routeAndProcessService.uppercase(message);
+         };
+    }
+
+
 }
