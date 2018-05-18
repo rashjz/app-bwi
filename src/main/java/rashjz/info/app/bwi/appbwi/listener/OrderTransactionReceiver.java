80a4d03c9ebf0aa4251c54d92228bee5057e6d2f
Rashad Javadov <rashjz@users.noreply.github.com>
2018-05-18 16:16:15 +0200

    Create OrderTransactionReceiver.java

---
+++ src/main/java/rashjz/info/app/bwi/appbwi/listener/OrderTransactionReceiver.java
@@ -0,0 +1,22 @@
+package rashjz.info.app.bwi.appbwi.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+import rashjz.info.app.bwi.appbwi.domain.OrderDetail;
+
+
+@Slf4j
+@Component
+public class OrderTransactionReceiver {
+
+    public static final String HELLO_QUEUE = "hello.queue";
+
+
+    @JmsListener(destination = HELLO_QUEUE, containerFactory = "jmsListenerContainerFactory")
+    public void receiveMessage(@Payload final OrderDetail message) {
+        log.info("Received < < < < <" + message.toString() + ">");
+
+    }
+}
