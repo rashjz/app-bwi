b90a08b627208141f3d4c139d1024e68dd82b227
Rashad Javadov <rashjz@users.noreply.github.com>
2018-05-29 13:47:08 +0200

    Create AsyncConfig.java

---
+++ src/main/java/rashjz/info/app/bwi/appbwi/config/AsyncConfig.java
@@ -0,0 +1,39 @@
+package rashjz.info.app.bwi.appbwi.config;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.stream.Stream;
+
+@Configuration
+@Slf4j
+@EnableAsync
+public class AsyncConfig implements AsyncConfigurer {
+
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(5);
+        executor.setQueueCapacity(15);
+        executor.setThreadNamePrefix("Async-");
+        executor.initialize();
+        return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return (throwable, method, objects) -> {
+            StringBuilder sb = new StringBuilder();
+            Stream.of(objects).forEach(sb::append);
+            log.error("Method Name {} params={}", method, sb, throwable);
+        };
+    }
+}
