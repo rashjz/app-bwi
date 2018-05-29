6a10ecff6e21ce0740e794b70b56106554aa02a7
Rashad Javadov <rashjz@users.noreply.github.com>
2018-05-29 13:49:42 +0200

    Update MainIntegrationConfig.java

--- src/main/java/rashjz/info/app/bwi/appbwi/config/MainIntegrationConfig.java
+++ src/main/java/rashjz/info/app/bwi/appbwi/config/MainIntegrationConfig.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.dsl.IntegrationFlow;
@@ -12,7 +13,9 @@ import org.springframework.integration.dsl.channel.MessageChannels;
 import org.springframework.integration.handler.LoggingHandler;
 import org.springframework.integration.jms.dsl.Jms;
 import org.springframework.integration.scheduling.PollerMetadata;
+import org.springframework.integration.util.CallerBlocksPolicy;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.messaging.MessageHandler;
 import rashjz.info.app.bwi.appbwi.listener.OrderTransactionReceiver;
 
@@ -53,6 +56,7 @@ public class MainIntegrationConfig {
     @Bean
     public IntegrationFlow jmsOutboundGatewayFlow() {
         return from(requestChannel())
+                .channel(MessageChannels.executor("httpTaskExecutorChannel", httpTaskExecutor()))
                 .handle(Jms.outboundAdapter(connectionFactory())
                         .destination(OrderTransactionReceiver.HELLO_QUEUE))
                 .get();
@@ -70,5 +74,16 @@ public class MainIntegrationConfig {
         logger.setShouldLogFullMessage(true);
         return logger;
     }
-
+    
+    @Bean
+    public TaskExecutor httpTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(5);
+        executor.setQueueCapacity(15);
+        executor.setDaemon(true);
+        executor.setThreadNamePrefix("Async-");
+        executor.setRejectedExecutionHandler(new CallerBlocksPolicy(10));
+        return executor;
+    }
 }
