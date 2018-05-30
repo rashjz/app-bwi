2c9f55a055cecda585efc94fe5a1920a041f47f6
Rashad Javadov <rashjz@users.noreply.github.com>
2018-05-30 13:20:55 +0200

    OrderTransactionReceiver.java updated
    @ExtensionMethod added for date utils

--- src/main/java/rashjz/info/app/bwi/appbwi/listener/OrderTransactionReceiver.java
+++ src/main/java/rashjz/info/app/bwi/appbwi/listener/OrderTransactionReceiver.java
@@ -1,14 +1,19 @@
 package rashjz.info.app.bwi.appbwi.listener;
 
+import lombok.experimental.ExtensionMethod;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 import rashjz.info.app.bwi.appbwi.domain.OrderDetail;
+import rashjz.info.app.bwi.appbwi.utils.DateUtils;
+
+import static rashjz.info.app.bwi.appbwi.utils.DateUtils.toLocalDate;
 
 
 @Slf4j
 @Component
+@ExtensionMethod({DateUtils.class})
 public class OrderTransactionReceiver {
 
     public static final String HELLO_QUEUE = "hello.queue";
@@ -16,7 +21,8 @@ public class OrderTransactionReceiver {
 
     @JmsListener(destination = HELLO_QUEUE, containerFactory = "jmsListenerContainerFactory")
     public void receiveMessage(@Payload final OrderDetail message) {
-        log.info("Received < < < < <" + message.toString() + "> > > > >");
+        message.setTransactionDate(toLocalDate(message.getTransactionDate()));
+        log.info("Received < < < < <" + message.toString() + ">");
 
     }
 }
